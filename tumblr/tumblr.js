a3fe03ebb5ca522f8b0c6e85a5d0a314863fd615
Chris Bergeron <epyon_avenger@chaos-axiom.com>
2013-05-13 11:21:55 -0700

    Tweet fixes again.
    Still don't know why we need that.

--- tumblr/tumblr.js
+++ tumblr/tumblr.js
@@ -5,7 +5,7 @@
 		console.log(query);
 
 		var d = new Date();
-		var ts = d.getTime();
+	  var ts = d.getTime();
 
 		var w = window.open('backend.php?op=backend&method=loading', 'ttrss_tweet',
 			"status=0,toolbar=0,location=0,width=500,height=400,scrollbars=1,menubar=0");
@@ -25,7 +25,7 @@
 
 
 	} catch (e) {
-		exception_error("tumblrArticle", e);
+		exception_error("tweetArticle", e);
 	}
 	}
 
