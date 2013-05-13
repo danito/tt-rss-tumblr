863c35d8b0d240afd5ad946d36c362640480e3bb
Chris Bergeron <epyon_avenger@chaos-axiom.com>
2013-05-13 11:19:15 -0700

    More fixes.
    I dunno why it needs ttrss_tweet, but it does.

--- tumblr/tumblr.js
+++ tumblr/tumblr.js
@@ -7,7 +7,7 @@
 		var d = new Date();
 		var ts = d.getTime();
 
-		var w = window.open('backend.php?op=backend&method=loading', 'ttrss_tumblr',
+		var w = window.open('backend.php?op=backend&method=loading', 'ttrss_tweet',
 			"status=0,toolbar=0,location=0,width=500,height=400,scrollbars=1,menubar=0");
 
 		new Ajax.Request("backend.php",	{
@@ -15,9 +15,9 @@
 			onComplete: function(transport) {
 				var ti = JSON.parse(transport.responseText);
 
-				var share_url = "http://www.tumblr.com/share?v=3" +
-					"&u=" + param_escape(ti.link) +
-					"&t=" + param_escape(ti.title);
+				var share_url = "http://www.tumblr.com/share?s=&v=3" +
+					"&t=" + param_escape(ti.title) +
+					"&u=" + param_escape(ti.link);
 
 				w.location.href = share_url;
 
